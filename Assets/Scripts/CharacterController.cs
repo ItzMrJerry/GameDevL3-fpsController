52d7bd2012798334c137b3c0564ec3f71a47b352
ItzMrJerry <jerrykrekels@gmail.com>
2023-02-06 17:36:32 +0100

    CharacterControler Force version added

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     [Header("Movement settings")]
-    [SerializeField] float sprintSpeed;
-    [SerializeField] float walkSpeed, jumpForce, smoothTime;
+    [SerializeField] float jumpForce = 250f;
+    [SerializeField] float walkSpeed = 6f, sprintSpeed = 8f;
+    [Range(0, 1f)]
+    [SerializeField] float smoothTime = 0.15f;
 
-    public bool isgrounded;
+    [Space]
+    public MovementTypeEnum MovementType = new MovementTypeEnum();
+    [Header("Only effects movement while using Force")]
+    [SerializeField] float maximumSpeed = 5f;
+
+    [ReadOnlyInspector]
+    public bool isGrounded;
     Vector3 SmoothMoveVelocity;
     Vector3 moveAmount;
     Vector3 moveDir;
@@ -28,23 +36,76 @@ public class CharacterController : MonoBehaviour
     private void Move()
     {
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref SmoothMoveVelocity, smoothTime);
+            
+
+
+        switch (MovementType)
+        {
+            case MovementTypeEnum.MovePosition:
+                moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref SmoothMoveVelocity, smoothTime);
+                break;
+            case MovementTypeEnum.Force:
+
+                    float speed = Vector3.Magnitude(rb.velocity);  
+                    float brakeSpeed = speed - maximumSpeed;
+
+                    Vector3 normalisedVelocity = rb.velocity.normalized;
+                    Vector3 brakeVelocity = normalisedVelocity * brakeSpeed;
+
+                if (speed > maximumSpeed)
+                {
+                    rb.AddForce(-brakeVelocity);
+                }
+
+                //Reduce sliding when not being controlled.
+                //if (isGrounded && speed > 0 && speed != 0 && moveDir.z == 0 && moveDir.x == 0)
+                //{
+                //    rb.velocity = rb.velocity * 0.9f;
+                   
+                //}
+                if (isGrounded && speed > 0 && speed != 0 && moveDir.z == 0 && moveDir.x == 0)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x * 0.9f, rb.velocity.y, rb.velocity.z * 0.9f);
+                }
+                //if (isGrounded && speed > 0 && speed != 0 && moveDir.x == 0)
+                //{
+                //    rb.velocity = new Vector3(rb.velocity.x * 0.9f, rb.velocity.y, rb.velocity.z * 0.9f);
+                //}
+                break;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.deltaTime);
+        switch(MovementType)
+        {
+            case MovementTypeEnum.MovePosition:
+                rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+                break;
+            case MovementTypeEnum.Force:
+                Vector3 movedir = transform.TransformDirection(moveDir) * ((Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed) * 2) * Time.fixedDeltaTime;
+                rb.AddForce(movedir, ForceMode.Impulse);
+                
+                break;
+        }
+
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(transform.up * jumpForce);
         }
     }
     public void SetGroundedSate(bool _grounded)
     {
-        isgrounded = _grounded;
+        isGrounded = _grounded;
     }
 }
+
+public enum MovementTypeEnum
+{
+    MovePosition,
+    Force
+};
